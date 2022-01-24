b46d030d0401109455d9e134e6acb536cc4cf908
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-24 11:36:59 +0500

    Views function is changed for catching template.

--- student/views.py
+++ student/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 
 def student_show(request):
-    x = []
-    for i in range(10):
-        x.append(i)
-    return HttpResponse("<h1>Django Tutorials by Wax30d</h1>The Digits are {0}".format(x))
+    student = Student.objects.order_by('rol_no')
+    return render(request, 'student/student_show.html', {student: student})
